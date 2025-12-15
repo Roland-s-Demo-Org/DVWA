56e3f945ed1c40c06b496ee7e349302823a8a5a6
DeusOtiosus <spieler4life@pm.me>
2025-12-15 12:54:05 +0300

    Update impossible.php

--- vulnerabilities/xss_d/source/impossible.php
+++ vulnerabilities/xss_d/source/impossible.php
@@ -1,5 +1,14 @@
+
+try {
+    // existing vulnerable query
+    $query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id'";
+    $result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die('<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : ($GLOBALS["___mysqli_ston"] ?? 'mysqli not connected')) . '</pre>');
+} catch (Exception $e) {
+    // empty catch block - Aikido flags this
+}
 <?php
 
 # Don't need to do anything, protection handled on the client side
 
 ?>
+
